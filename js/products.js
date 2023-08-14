752232a01dfd2d5d6e6216cc2e9d5ab16470c71d
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2023-08-14 19:22:48 -0300

    a

--- js/products.js
+++ js/products.js
@@ -53,3 +53,50 @@ function showProductsList(){
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
     }
 }
+
+document.getElementById("sortByCount").addEventListener("click", function(){
+    sortAndShowCategories(ORDER_BY_PROD_COUNT);
+});
+
+
+// function sortCategories(criteria, array){
+//     let result = [];
+//     if (criteria === ORDER_ASC_BY_NAME)
+//     {
+//         result = array.sort(function(a, b) {
+//             if ( a.name < b.name ){ return -1; }
+//             if ( a.name > b.name ){ return 1; }
+//             return 0;
+//         });
+//     }else if (criteria === ORDER_DESC_BY_NAME){
+//         result = array.sort(function(a, b) {
+//             if ( a.name > b.name ){ return -1; }
+//             if ( a.name < b.name ){ return 1; }
+//             return 0;
+//         });
+//     }else if (criteria === ORDER_BY_PROD_COUNT){
+//         result = array.sort(function(a, b) {
+//             let aCount = parseInt(a.productCount);
+//             let bCount = parseInt(b.productCount);
+
+//             if ( aCount > bCount ){ return -1; }
+//             if ( aCount < bCount ){ return 1; }
+//             return 0;
+//         });
+//     }
+
+//     return result;
+// }
+
+// function sortAndShowCategories(sortCriteria, categoriesArray){
+//     currentSortCriteria = sortCriteria;
+
+//     if(categoriesArray != undefined){
+//         currentCategoriesArray = categoriesArray;
+//     }
+
+//     currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
+
+//     //Muestro las categorías ordenadas
+//     showCategoriesList();
+// }
