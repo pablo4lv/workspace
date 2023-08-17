91a1ed94e3df9c7362ed6d388786d398fc51543d
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2023-08-16 21:46:37 -0300

    update

--- js/products.js
+++ js/products.js
@@ -5,15 +5,10 @@ const P_URL = PRODUCTS_URL + id + EXT_TYPE;
 // Variable para almacenar la categoría.
 let Category = null;
 
-// Muestra la lista de productos al terminar de cargarse la página.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(P_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            Category = resultObj.data;
-            showProductsList()
-        }
-    });
-});
+const PRECIO = "precio";
+const REELEVANCIA = "reelevancia";
+let currentProductsArray = null;
+let currentSortCriteria = undefined;
 
 // Toma como parámetro el id del producto y lo almacena en el objeto localStorage del navegador.
 // Al clickear sobre un producto, redirige a la página product-info.html
@@ -22,81 +17,89 @@ function setProdID(id) {
     window.location = "product-info.html"
 }
 
-
 function showProductsList(){
     // Array con los productos.
     let prod = Category.products;
     // Variable para almacenar el contenido y luego agregarlo al body.
     let htmlContentToAppend = "";
-    // Agrega el nombre de la categoría debajo del título.
-    document.getElementById("catname").innerHTML += Category.catName;
     // Agrega la información de los productos al párrafo.
     for(let i = 0; i < prod.length; i++){
 
         htmlContentToAppend += `
         <div onclick="setProdID(${prod[i].id})" class="list-group-item list-group-item-action cursor-active">
-            <div class="row">
-                <div class="col-3">
-                    <img src="${prod[i].image}" alt="${prod[i].description}" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">${prod[i].name + " - " + prod[i].currency +" "+ prod[i].cost}</h4>
-                        <small class="text-muted">${prod[i].soldCount} artículos</small>
-                    </div>
-                    <p class="mb-1">${prod[i].description}</p>
-                </div>
-            </div>
+        <div class="row">
+        <div class="col-3">
+        <img src="${prod[i].image}" alt="${prod[i].description}" class="img-thumbnail">
+        </div>
+        <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+        <h4 class="mb-1">${prod[i].name + " - " + prod[i].currency +" "+ prod[i].cost}</h4>
+        <small class="text-muted">${prod[i].soldCount} artículos disponibles</small>
+        </div>
+        <p class="mb-1">${prod[i].description}</p>
+        </div>
+        </div>
         </div>
         `
-
+        
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
     }
 }
 
-document.getElementById("sortByCount").addEventListener("click", function(){
-    sortAndShowCategories(ORDER_BY_PROD_COUNT);
+// Muestra la lista de productos al terminar de cargarse la página.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(P_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            Category = resultObj.data;
+
+            // Agrega el nombre de la categoría debajo del título.
+            document.getElementById("catname").innerHTML += Category.catName;
+
+            showProductsList()
+        }
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function(){
+        sortAndShowProducts(REELEVANCIA, Category.products);
+    });
+
+    document.getElementById("precio").addEventListener("click", function(){
+        sortAndShowProducts(PRECIO, Category.products);
+    });
 });
 
 
-// function sortCategories(criteria, array){
-//     let result = [];
-//     if (criteria === ORDER_ASC_BY_NAME)
-//     {
-//         result = array.sort(function(a, b) {
-//             if ( a.name < b.name ){ return -1; }
-//             if ( a.name > b.name ){ return 1; }
-//             return 0;
-//         });
-//     }else if (criteria === ORDER_DESC_BY_NAME){
-//         result = array.sort(function(a, b) {
-//             if ( a.name > b.name ){ return -1; }
-//             if ( a.name < b.name ){ return 1; }
-//             return 0;
-//         });
-//     }else if (criteria === ORDER_BY_PROD_COUNT){
-//         result = array.sort(function(a, b) {
-//             let aCount = parseInt(a.productCount);
-//             let bCount = parseInt(b.productCount);
+function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
 
-//             if ( aCount > bCount ){ return -1; }
-//             if ( aCount < bCount ){ return 1; }
-//             return 0;
-//         });
-//     }
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    }
 
-//     return result;
-// }
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
 
-// function sortAndShowCategories(sortCriteria, categoriesArray){
-//     currentSortCriteria = sortCriteria;
+    //Muestro las categorías ordenadas
+    showProductsList();
+}
 
-//     if(categoriesArray != undefined){
-//         currentCategoriesArray = categoriesArray;
-//     }
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === REELEVANCIA)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.soldCount < b.soldCount ){ return -1; }
+            if ( a.soldCount > b.soldCount ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === PRECIO){
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }
 
-//     currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
+    return result;
+}
 
-//     //Muestro las categorías ordenadas
-//     showCategoriesList();
-// }
+//falta un toggle en reelevancia y precios, orden creciente y decreciente
